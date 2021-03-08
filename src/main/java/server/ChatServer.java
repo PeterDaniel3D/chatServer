928977accd75dfd9669e55f2f5ae24df8b251d3e
PeterDaniel3D <75070076+PeterDaniel3D@users.noreply.github.com>
2021-03-08 20:36:47 +0100

    Merged Daniels chatdemo with Lars start-code

--- src/main/java/server/ChatServer.java
+++ src/main/java/server/ChatServer.java
@@ -1,18 +1,28 @@
 package server;
 
-
-import java.net.InetAddress;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.UnknownHostException;
+import java.net.Socket;
+import java.util.Vector;
 
 public class ChatServer {
 
+    // Vector to store active clients
+    static Vector<ClientHandler> ar = new Vector<>();
+
+    // counter for clients
+    static int i = 0;
 
     //Call server with arguments like this: 0.0.0.0 8088 logfile.log
-    public static void main(String[] args) throws UnknownHostException {
-        String ip ="localhost";
-        int port = 8088;
-        String logFile = "log.txt";  //Do we need this
+    public static void main(String[] args) throws IOException {
+
+        //TODO: Hardcoded list with users
+
+        String ip;
+        int port;
+        String logFile;
 
         try {
             if (args.length == 3) {
@@ -28,7 +38,46 @@ public class ChatServer {
             return;
         }
 
+        //TODO: Create Server
+
+        // server is listening on port 1234
+        ServerSocket ss = new ServerSocket(1234);
+
+        Socket s;
+
+        // running infinite loop for getting
+        // client request
+        while (true) {
+            // Accept the incoming request
+            s = ss.accept();
+
+            System.out.println("New client request received : " + s);
+
+            // obtain input and output streams
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+            System.out.println("Creating a new handler for this client...");
+
+            // Create a new handler object for handling this request.
+            ClientHandler mtch = new ClientHandler(s, "client " + i, dis, dos);
+
+            // Create a new Thread with this object.
+            Thread t = new Thread(mtch);
+
+            System.out.println("Adding this client to active client list");
+
+            // add this client to active clients list
+            ar.add(mtch);
+
+            t.setName("Peter");
+            // start the thread.
+            t.start();
+
+            // increment i for new client.
+            // i is used for naming only, and can be replaced
+            // by any naming scheme
+            i++;
+        }
     }
-
-
 }
